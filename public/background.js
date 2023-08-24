786ed13092f470f15b666f5b89ff40848b21b194
Vaishnav <vaishnavdixit98@gmail.com>
2023-08-25 03:10:58 +0530

    onChange Listener fixed.

--- public/background.js
+++ public/background.js
@@ -1,12 +1,29 @@
 console.log("hello from background.js");
-chrome.storage.local.set({
-    "mode": "stopped",
-    "time": 15*60,
-    "timePresets": {"time": 15*60},
-    "timeSelected": 1,
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Installed");
+    chrome.storage.local.set(
+        {
+            "mode": "stopped",
+            "time": 15 * 60,
+            "timePresets": {"time": 15 * 60},
+            "timeSelected": 1,
+        },
+        () => {
+            console.log("set BG.js after installing.");
+        }
+    );
 });
+
 chrome.alarms.onAlarm.addListener((alarm) => {
+    console.log("alarm hits");
     chrome.storage.local.get(["time"], (res) => {
-        chrome.storage.local.set({"time": res.time - 1});
+        console.log("get in BG.js, res=", res);
+        chrome.storage.local.set({"time": res.time - 1}, () => {
+            chrome.action.setBadgeText({
+                text: `${Math.floor(res.time / 60)}:${
+                    res.time % 60
+                }`,
+            });
+        });
     });
 });
--- src/App.js
+++ src/App.js
@@ -3,70 +3,96 @@
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Button, ProgressBar} from "react-bootstrap";
+import {
+    faPlay,
+    faCheck,
+    faStop,
+} from "@fortawesome/free-solid-svg-icons";
 import "./App.scss";
-import {useState, useTransition} from "react";
+import {useEffect, useState, useTransition} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function App() {
     console.log("popup start...");
+    const [allStorage, setAllStorage] = useState({});
     const [time, setTime] = useState();
     const [initialTime, setInitialTime] = useState(0);
     const [displayMode, setDisplayMode] = useState("stopped");
     const [isEditing, setIsEditing] = useState(false);
     const [toChangeTime, setToChangeTime] = useState(0);
     const [radioSelected, setRadioSelected] = useState(-1);
-    chrome.storage.local.get(
-        ["mode", "timePresets", "time", "timeSelected"],
-        (res) => {
-            console.log(
-                "get Function fired, res =",
-                JSON.stringify(res)
-            );
-            setDisplayMode(res.mode);
-            setTime(res.time || 0);
-            setInitialTime(res.timePresets.time);
-            setRadioSelected(res.timeSelected);
-        }
-    );
+    useEffect(() => {
+        chrome.storage.local.get(
+            ["mode", "time", "timeSelected", "timePresets"],
+            (res) => {
+                console.log(
+                    "get Function fired, res =",
+                    JSON.stringify(res)
+                );
+                setAllStorage(res);
+                setDisplayMode(res.mode);
+                setTime(res.time || 0);
+                setInitialTime(res.timePresets.time);
+                setRadioSelected(res.timeSelected);
+            }
+        );
+        chrome.storage.onChanged.addListener((changes, namespace) => {
+            console.log("changes, changes=", changes);
+            if (changes.time) {
+                if (changes.time.newValue == 0)
+                    chrome.alarms.clear("alarm", () => {
+                        console.log(56);
+                        chrome.storage.local.set(
+                            {
+                                "mode": "stopped",
+                                "time": initialTime,
+                            },
+                            () => {
+                                console.log("time==0, stopping.");
+                            }
+                        );
+                    });
+
+                setTime(changes.time.newValue);
+            }
+            if (changes.mode) setDisplayMode(changes.mode.newValue);
+            if (changes.timePresets)
+                setInitialTime(changes.timePresets.newValue.time);
+        });
+    }, []);
 
     const radioOnClickListener = (t) => {
         console.log("radio clicked, t=", t);
-        chrome.storage.local.set({"time": t});
+        chrome.storage.local.set({"time": t}, () => {
+            console.log("radio clicked. data updated.");
+        });
     };
 
-    chrome.storage.onChanged.addListener((changes, namespace) => {
-        console.log(
-            "onChange fired, changes=",
-            JSON.stringify(changes)
-        );
-        if (changes.time) {
-            if (changes.time.newValue == 0)
-                // timer over
-                chrome.alarms.clear("alarm", () => {
-                    chrome.storage.local.set({
-                        "mode": "stopped",
-                        "time": initialTime,
-                    });
-                });
-            setTime(changes.time.newValue);
-        }
-        // if (changes.mode) {
-        //     setDisplayMode(changes.mode.newValue);
-        // }
-    });
+    console.log("before onChange line");
+
+    console.log("after onChange line");
 
     const start = () => {
         console.log("start Clicked.");
         chrome.alarms.create(
             "alarm",
             {
-                periodInMinutes: 1 / 60,
+                periodInMinutes: 1.0 / 60,
             },
             () => {
-                setDisplayMode("started");
-                chrome.storage.local.set({
-                    "mode": "started",
-                    "time": initialTime,
-                });
+                chrome.storage.local.set(
+                    {
+                        "mode": "started",
+                        "time": initialTime,
+                    },
+                    () => {
+                        console.log(
+                            "values set after clicking start."
+                        );
+                        setDisplayMode("started");
+                        setIsEditing(false);
+                    }
+                );
             }
         );
     };
@@ -74,10 +100,15 @@ function App() {
     const stop = () => {
         console.log("stop Clicked.");
         chrome.alarms.clear("alarm", () => {
-            chrome.storage.local.set({
-                "mode": "stopped",
-                "time": initialTime,
-            });
+            chrome.storage.local.set(
+                {
+                    "mode": "stopped",
+                    "time": initialTime,
+                },
+                () => {
+                    console.log("stop clicked. alarm cleared.");
+                }
+            );
         });
     };
 
@@ -87,29 +118,32 @@ function App() {
 
     const doneClickHandler = () => {
         setIsEditing(false);
-        if (toChangeTime >= 0 && toChangeTime < 200 * 60) {
-            chrome.storage.local.set({
-                "time": toChangeTime,
-                "timePresets": {"time": toChangeTime},
-            });
-        }
     };
 
     const inputOnChangeListener = (value) => {
-        setToChangeTime(value * 60);
+        // setToChangeTime(value * 60);
+        if (value > 0 && value < 200) {
+            chrome.storage.local.set(
+                {
+                    "time": value * 60,
+                    "timePresets": {"time": value * 60},
+                },
+                () => {
+                    console.log("edit done, data updated.");
+                }
+            );
+        }
     };
 
-    const cancelClickHandler = () => {
-        setIsEditing(false);
-    };
+    console.log("popup End");
 
     return (
         <div className="App">
             <p className="text-center timerHeading">Timer</p>
             {displayMode != "started" ? (
                 <>
-                    <div>
-                        <input
+                    <div className="d-flex justify-content-center mb-3">
+                        {/* <input
                             className="radioBtn"
                             type="radio"
                             id="idTime1"
@@ -118,7 +152,7 @@ function App() {
                             onClick={() =>
                                 radioOnClickListener(initialTime)
                             }
-                        />
+                        /> */}
                         {isEditing ? (
                             <input
                                 type="number"
@@ -135,59 +169,57 @@ function App() {
                             </label>
                         )}
                     </div>
-                    {isEditing ? (
-                        <>
-                            <button
-                                type="button"
-                                className="btn btn-sm btn-success"
-                                onClick={doneClickHandler}
-                            >
-                                Done
-                            </button>
-                            <button
-                                type="button"
-                                className="btn btn-sm btn-outline-warning"
-                                onClick={cancelClickHandler}
-                            >
-                                Cancel
-                            </button>
-                        </>
-                    ) : (
-                        <div className="d-flex justify-content-center">
-                            <button
-                                type="button"
-                                className="btn btn-sm btn-success me-2"
-                                onClick={start}
-                                disabled={!time}
-                            >
-                                Start
-                            </button>
-                            <button
-                                type="button"
-                                className="btn btn-sm btn-warning"
-                                onClick={editClickHandler}
-                            >
-                                Edit
-                            </button>
-                        </div>
-                    )}
+                    <div className="d-flex justify-content-center">
+                        <button
+                            type="button"
+                            className="btn btn-sm btn-success me-2"
+                            onClick={start}
+                        >
+                            <FontAwesomeIcon icon={faPlay} /> Start
+                        </button>
+                        {isEditing ? (
+                            <>
+                                <button
+                                    type="button"
+                                    className="btn btn-sm btn-outline-primary"
+                                    onClick={doneClickHandler}
+                                >
+                                    <FontAwesomeIcon icon={faCheck} />{" "}
+                                    Done
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                                <button
+                                    type="button"
+                                    className="btn btn-sm btn-warning"
+                                    onClick={editClickHandler}
+                                >
+                                    Edit
+                                </button>
+                            </>
+                        )}
+                    </div>
                 </>
             ) : (
                 <>
-                    <button
-                        type="button"
-                        className="btn btn-sm btn-warning ms-2"
-                        onClick={stop}
-                    >
-                        stop
-                    </button>
                     <ProgressBar
                         striped
                         now={((time / initialTime) * 100).toFixed(2)}
+                        label={`${Math.floor(time / 60)}m ${
+                            time % 60
+                        }s`}
                     />
+                    <button
+                        type="button"
+                        className="btn btn-sm btn-danger mb-3 mx-auto d-block"
+                        onClick={stop}
+                    >
+                        <FontAwesomeIcon icon={faStop} /> Stop
+                    </button>
                 </>
             )}
-            <p className="mt-2 mb-0">time: {time}</p>
+            {/* <p className="mt-2 mb-0">time: {time}</p>
             <p className="mb-0">initialTime: {initialTime}</p>
             <p className="mb-0">displayMode: {displayMode}</p>
             <p className="mb-0">isEditing: {isEditing}</p>
@@ -196,6 +228,9 @@ function App() {
                 ProgressBar percentage:{" "}
                 {((time / initialTime) * 100).toFixed(2)}
             </p>
+            <p style={{whiteSpace: "pre-wrap"}}>
+                allStorage: {JSON.stringify(allStorage)}
+            </p> */}
         </div>
     );
 }
