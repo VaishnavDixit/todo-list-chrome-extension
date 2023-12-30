d28b2e70a292a428473996559c75e348c9e5dedd
Vaishnav <vaishnavdixit98@gmail.com>
2023-12-31 00:04:21 +0530

    advances

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 
 import {
     faCheck,
+    faChevronRight,
     faPlus,
 } from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
@@ -13,13 +14,19 @@ import "./App.scss";
 function App() {
     const {v4: uuidv4} = require("uuid");
     const [tasks, setTasks] = useState([]);
-    useEffect(() => {
-        chrome.storage.sync.get(
-            ["tasks"],
-            (res) => {
-                setTasks(res.tasks || []);
-            }
-        );
+	const list = new Map();
+	list["1"]=["11","12","13"];
+	list["2"]=["21","22","23","24"];
+	list["3"]=["31","33"];
+	list["12"]=["121","122"];
+
+	useEffect(() => {
+        // chrome.storage.sync.get(
+        //     ["tasks"],
+        //     (res) => {
+        //         setTasks(res.tasks || []);
+        //     }
+        // );
     }, []);
 
     const addClickHandler = () => {
@@ -36,9 +43,9 @@ function App() {
             ...tasks,
         ];
         setTasks(newTasksList);
-        chrome.storage.sync.set({
-            tasks: newTasksList,
-        });
+        // chrome.storage.sync.set({
+        //     tasks: newTasksList,
+        // });
     };
 
     const deleteClickHandler = (uuid) => {
@@ -48,15 +55,15 @@ function App() {
             ),
         ];
         setTasks(temp);
-        chrome.storage.sync.set({
-            tasks: temp,
-        });
+        // chrome.storage.sync.set({
+        //     tasks: temp,
+        // });
     };
 
     return (
         <div
             className="App p-2 pe-0 pb-0"
-            style={{width: "350px"}}
+            style={{width: "600px"}}
         >
             <Row className="mb-1 d-flex justify-content-between px-2">
                 <Col xs={6} className="ps-1">
@@ -66,7 +73,7 @@ function App() {
                 </Col>
             </Row>
             <Row className="pe-2 mb-1">
-                <Col xs={9} className="pe-0">
+                <Col xs={8} className="pe-0">
                     <input
                         type="text"
                         className="form-control"
@@ -92,25 +99,23 @@ function App() {
             </Row>
             <Row className="tasksListRow mx-0">
                 <Col
-                    xs={10}
+                    xs={1}
+                    className="pe-0 d-flex align-items-center justify-content-center"
+                >
+                    <FontAwesomeIcon
+                        icon={faChevronRight}
+                        className="me-1"
+                    />
+                </Col>
+                <Col
+                    xs={11}
                     className="px-0 ps-1 task"
                 >
-                    <textarea type="text" id="fname"/>
-                </Col>
-                <Col xs={2}>
-                    <Button
-                        size="sm"
-                        variant="outline-success"
-                        // onClick={() =>
-                        //     deleteClickHandler(
-                        //         val.uuid
-                        //     )
-                        // }
-                    >
-                        <FontAwesomeIcon
-                            icon={faCheck}
-                        />
-                    </Button>
+                    <textarea
+                        type="text"
+                        id="fname"
+						rows={1}
+                    />
                 </Col>
             </Row>
         </div>
